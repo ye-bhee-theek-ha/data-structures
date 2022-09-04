701f3e39c112c27e63084978591b4d9d63482bac
ye-bhee-theek-ha <88560890+ye-bhee-theek-ha@users.noreply.github.com>
2022-09-04 22:30:33 +0500

    Merge sort added

--- data_structures/sorting.cpp
+++ data_structures/sorting.cpp
@@ -72,7 +72,71 @@ void selection_sort(int* arr, int len)
 
 }
 
-void merge_sort(int* arr, int len)
+void merge(int* arr, int* arr_left, int size_left, int* arr_right, int size_right)
 {
+	int arr_counter = 0;
+	int left_counter = 0;
+	int right_counter = 0;
+
+	while (left_counter < size_left && right_counter < size_right)
+	{
+		if (arr_left[left_counter] <= arr_right[right_counter])
+		{
+			arr[arr_counter] = arr_left[left_counter];
+			left_counter++;
+		}
+		else
+		{
+			arr[arr_counter] = arr_right[right_counter];
+			right_counter++;
+		}
+		arr_counter++;
+	}
+
+	while (left_counter < size_left)
+	{
+		arr[arr_counter] = arr_left[left_counter];
+		left_counter++;
+		arr_counter++;
+	}
+
+	while (right_counter < size_right)
+	{
+		arr[arr_counter] = arr_right[right_counter];
+		right_counter++;
+		arr_counter++;
+	}
+}
+
+
+void  merge_sort(int* arr, int len)
+{
+	if (len == 1)
+	{
+		return;
+	}
+
+	int mid = len / 2;
+	int len_left = mid;
+	int len_right = len - mid;
+
+	int* left = new int[len_left];
+	int* right = new int[len_right];
+
+	for (int i = 0; i < mid; i++)
+	{
+		left[i] = arr[i];
+	}
+	for (int i = mid, j = 0; i < len; i++,j++)
+	{
+		right[j] = arr[i];
+	}
+
+	merge_sort(left, len_left);
+	merge_sort(right, len_right);
+
+
+	merge(arr, left, len_left, right, len_right);
+
 
 }
